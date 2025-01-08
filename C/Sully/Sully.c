f64b0e472991222c50bbe86bf990e092fb3b7f0e
Vassili <arragois@gmail.com>
2025-01-08 01:42:05 +0100

    Now Sully Works perfectly

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char src[] = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$cint main()%2$c{%2$c%1$cchar src[] = %3$c%4$s%3$c;%2$c%1$cint i = %5$d;%2$c%1$cchar name[10], path[10], command[50];%2$c#ifdef KID%2$c%1$ci--;%2$c#endif%2$c%1$cif (i < 0)%2$c%1$c%1$creturn 0;%2$c%1$csprintf(name, %3$cSully_%6$cd%3$c, i);%2$c%1$csprintf(path, %3$cSully_%6$cd.c%3$c, i);%2$c%1$cFILE *f = fopen(path, %3$cw%3$c);%2$c%1$cif (f == NULL)%2$c%1$c%1$creturn 1;%2$c%1$cfprintf(f, src, 9, 10, 34, src, i);%2$c%1$cfclose(f);%2$c%1$csprintf(command, %3$cgcc %6$cs -D KID -o %6$cs%3$c, path, name);%2$c%1$csystem(command);%2$c%1$creturn 0;%2$c}%2$c";
+	char src[] = "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$cint main()%2$c{%2$c%1$cchar src[] = %3$c%4$s%3$c;%2$c%1$cint i = %5$d;%2$c%1$cchar name[10], path[10], command[50];%2$c#ifdef KID%2$c%1$ci--;%2$c#endif%2$c%1$cif ( i < 0 )%2$c%1$c%1$creturn 0;%2$c%1$csprintf(name, %3$cSully_%6$cd%3$c, i);%2$c%1$csprintf(path, %3$cSully_%6$cd.c%3$c, i);%2$c%1$cFILE *f = fopen(path, %3$cw%3$c);%2$c%1$cif (f == NULL)%2$c%1$c%1$creturn 1;%2$c%1$cfprintf(f, src, 9, 10, 34, src, i, 37);%2$c%1$cfclose(f);%2$c%1$csprintf(command, %3$cgcc %6$c1$s -D KID -o %6$c2$s; ./%6$c2$s%3$c, path, name);%2$c%1$csystem(command);%2$c%1$creturn 0;%2$c}%2$c";
 	int i = 5;
 	char name[10], path[10], command[50];
 #ifdef KID
@@ -18,7 +18,7 @@ int main()
 		return 1;
 	fprintf(f, src, 9, 10, 34, src, i, 37);
 	fclose(f);
-	sprintf(command, "gcc %s -D KID -o %s", path, name);
+	sprintf(command, "gcc %1$s -D KID -o %2$s; ./%2$s", path, name);
 	system(command);
 	return 0;
 }
